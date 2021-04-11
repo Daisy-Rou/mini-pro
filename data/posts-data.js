1531e76a89ed54231a2195431f30409f42cf3663
huangrou <2098646597@qq.com>
2021-04-11 22:44:39 +0800

    feat: 1、全局组件引入 2、小程序单向数据绑定

---
+++ data/posts-data.js
@@ -0,0 +1,24 @@
+var local_database = [
+  {
+    title: "2020课程的计划ID",
+    content: "加速度场比赛成绩对你好城北街道睡不好觉，查到那会比读书包括，打开就是粗一个打死科技达成半句话付，接受程度和GV价格低客户端水泥厂，程度会比较不错就好好打不成被冻结，苍井空比较快递分部的刷卡缴费年初开始，吃的那家活动时间超过的，都是覅胡覅加快速度和你。",
+    imgSrc: "/images/padx.png",
+    reading: 102,
+    detail: "加速度场比赛成绩对你好城北街道睡不好觉，查到那会比读书包括，打开就是粗一个打死科技达成半句话付，接受程度和GV价格低客户端水泥厂，程度会比较不错就好好打不成被冻结，苍井空比较快递分部的刷卡缴费年初开始，吃的那家活动时间超过的，都是覅胡覅加快速度和你。",
+    collection: 92,
+    dateTime: "24小时前",
+    heeadImgSrc: '/images/padx.png',
+    author: "猫是猫的猫",
+    date: "Nov18 2020",
+    avatar: "/images/keai.png",
+    postId: 3,
+    music: {
+      url: "",
+      title: "cdj1",
+      coverImg: ""
+    }
+  }
+]
+module.exports = {
+  postList: local_database
+}
--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -5,14 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    a: '就开始你觉得好的借口'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(123)
   },
 
   /**
