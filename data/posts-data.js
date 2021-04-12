b4fa3112af579ad7fba25e771db10cbcdaaaa935
Daisy-Rou <2098646597@qq.com>
2021-04-12 12:17:12 +0800

    feat: 1、文章列表渲染

--- data/posts-data.js
+++ data/posts-data.js
@@ -7,7 +7,7 @@ var local_database = [
     detail: "加速度场比赛成绩对你好城北街道睡不好觉，查到那会比读书包括，打开就是粗一个打死科技达成半句话付，接受程度和GV价格低客户端水泥厂，程度会比较不错就好好打不成被冻结，苍井空比较快递分部的刷卡缴费年初开始，吃的那家活动时间超过的，都是覅胡覅加快速度和你。",
     collection: 92,
     dateTime: "24小时前",
-    heeadImgSrc: '/images/padx.png',
+    headImgSrc: '/images/padx.png',
     author: "猫是猫的猫",
     date: "Nov18 2020",
     avatar: "/images/keai.png",
