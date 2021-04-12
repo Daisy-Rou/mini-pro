9d002099ef706ffbdc37f484a8ceb0c73219b6aa
huangrou <2098646597@qq.com>
2021-04-12 23:26:04 +0800

    feat: 1、文章收藏未收藏状态逻辑编写

--- pages/post-detail/post-detail.js
+++ pages/post-detail/post-detail.js
@@ -8,17 +8,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    collected: false,
+    // 不做数据绑定前面加_
+    _pid: null,
+    _postsCollected: {}
   },
-
+  onCollect() {
+    // 假设为收藏->收藏
+    // 那篇文章被收藏
+    // 多篇文章被收藏
+    const postsCollected = this.data._postsCollected
+    postsCollected[this.data._pid] = !this.data.collected
+    this.setData({
+      collected: !this.data.collected
+    })
+    wx.setStorageSync('posts_collected', postsCollected)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     const pid = JSON.parse(options.pid)
+    this.data._pid = pid
+    // 从缓存中读取收藏状态
+    const postsCollected = wx.getStorageSync('posts_collected')
+    this.data._postsCollected = postsCollected
+    let collected = postsCollected[options.pid]
+    if (collected === undefined)  {
+      collected = false
+    }
+    this.setData({
+      collected,
+    })
     postList.forEach(item => {
       if (item.postId === pid) {
-        console.log(item.postId)
         this.setData({
           postList: item
         })
